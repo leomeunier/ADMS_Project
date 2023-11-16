3cb3406cd310386229173272a5fba34a3f4f35f8
leomeunier <90618703+leomeunier@users.noreply.github.com>
2023-11-16 17:40:53 +0100

    Add files via upload

--- script.m
+++ script.m
@@ -21,7 +21,9 @@ omega = 2*pi*fs;
 gamma = ((m*omega.^2)/(E*J)).^(1/4);
 
 det = (2 + 2*cos(gamma*L).*cosh(gamma*L)).*(gamma.^6);
-% semilogy(fs, abs(det))
+
+figure(1)
+semilogy(fs, abs(det))
 
 % Identification natural modes
 
@@ -40,21 +42,23 @@ for k = gammasol;
     X(i,:)= Xi;
 end
 
-x = 0:0.1:1.2;
+x = 0:0.01:1.2;
 
 Phi1 = phi(X(1,1),X(1,2),X(1,3),X(1,4),gammasol(1),x);
 Phi2 = phi(X(2,1),X(2,2),X(2,3),X(2,4),gammasol(2),x);
 Phi3 = phi(X(3,1),X(3,2),X(3,3),X(3,4),gammasol(3),x);
 Phi4 = phi(X(4,1),X(4,2),X(4,3),X(4,4),gammasol(4),x);
 
-% subplot(4,1,1)
-% plot(x,Phi1./max(abs(Phi1)))
-% subplot(4,1,2)
-% plot(x,Phi2./max(abs(Phi2)))
-% subplot(4,1,3)
-% plot(x,Phi3./max(abs(Phi3)))
-% subplot(4,1,4)
-% plot(x,Phi4./max(abs(Phi4)))
+
+figure(2)
+subplot(4,1,1)
+plot(x,Phi1./max(abs(Phi1)))
+subplot(4,1,2)
+plot(x,Phi2./max(abs(Phi2)))
+subplot(4,1,3)
+plot(x,Phi3./max(abs(Phi3)))
+subplot(4,1,4)
+plot(x,Phi4./max(abs(Phi4)))
 
 % Computing the FRF
 
@@ -79,10 +83,13 @@ Phi4k = phi(X(4,1),X(4,2),X(4,3),X(4,4),gammasol(4),xk);
 Gjk = ((Phi1j*Phi1k/m1)./(-omega.^2 + i*2*damp*omegasol(1)*omega + omegasol(1)^2)) + ((Phi2j*Phi2k/m2)./(-omega.^2 + i*2*damp*omegasol(2)*omega + omegasol(2)^2)) + ...
     ((Phi3j*Phi3k/m3)./(-omega.^2 + i*2*damp*omegasol(3)*fs + omegasol(3)^2)) + ((Phi4j*Phi4k/m4)./(-omega.^2 + i*2*damp*omegasol(4)*fs + omegasol(4)^2));
 
+figure(3)
 subplot(2,1,1)
 semilogy(fs,abs(Gjk))
 subplot(2,1,2)
 plot(fs,angle(Gjk))
+
+
 
 
 
