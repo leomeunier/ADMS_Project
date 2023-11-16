18ee61ef1847dc4a8d229cccac50c882eacf36d8
leomeunier <90618703+leomeunier@users.noreply.github.com>
2023-11-16 17:57:48 +0100

    Add files via upload

--- script.m
+++ script.m
@@ -31,15 +31,15 @@ fsol = [4.5 28.2 79 154.9];
 omegasol = 2*p*fsol;
 gammasol = ((m*omegasol.^2)/(E*J)).^(1/4);
 X = zeros(4,4);
-i = 0;
+v = 0;
 
 for k = gammasol;
-    i = i+1;
+    v = v+1;
     Hcap = [1 0 1;  -sin(k*L) cosh(k*L) sinh(k*L) ; -cos(k*L) sinh(k*L) cosh(k*L)];    
     N = [0 ; -cos(k*L) ;sin(k*L)];
     Xicap = -inv(Hcap)*N;
     Xi = [1 Xicap.'];
-    X(i,:)= Xi;
+    X(v,:)= Xi;
 end
 
 x = 0:0.01:1.2;
@@ -68,8 +68,8 @@ m2 = trapz(m*Phi2.^2);
 m3 = trapz(m*Phi3.^2);
 m4 = trapz(m*Phi4.^2);
 
-xk = 0.2;
-xj = 1.2;
+xk = 1.2;
+xj = 0.2;
 
 Phi1j = phi(X(1,1),X(1,2),X(1,3),X(1,4),gammasol(1),xj);
 Phi2j = phi(X(2,1),X(2,2),X(2,3),X(2,4),gammasol(2),xj);
