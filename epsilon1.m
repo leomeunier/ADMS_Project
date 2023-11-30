ee186cda9db2d3868f7086c8f1e4d1792d893969
Mirko1503 <150934162+Mirko1503@users.noreply.github.com>
2023-11-30 19:57:21 +0100

    Update epsilon1.m

--- epsilon1.m
+++ epsilon1.m
@@ -1,20 +1,20 @@
-function [result] = epsilon(x,fs,GrEXP)
+function [valuenm] = epsilon1(x,omegafs,GrEXP)
 
     % In x we store all the unknowns (Ai, dampi, omegai, Rh and Rl)
-    A = x(1:4);
-    damp = x(5);
-    omega = x(6);
-    Rhr = x(7);
-    Rhi=x(8);
-    Rlr=x(9);
-    Rli = x(10);
+    A = x(1);
+    damp = x(2);
+    omega = x(3);
+    Rhr = x(4);
+    Rhi=x(5);
+    Rlr=x(6);
+    Rli = x(7);
     % Result will store the sum of all number
-    result = 0;
-    omegafs = 2*pi*fs;
-    for n = 1:4
-        for m = 1:size(fs,2)
-            GrNUMnm = A(n)/(-omegafs(m)^2 +1i*2*damp*omega*omegafs(m) + omega^2) + (Rhr+1i*Rhi)/omegafs(m).^2 + Rlr+1i*Rli;
-            valuenm = real(GrEXP(n,m)- GrNUMnm)^2 + imag(GrEXP(n,m)- GrNUMnm)^2;
-            result = result + valuenm;
+    % result = 0;
+    % omegafs = 2*pi*fs;
+    for n = 1
+        for m = 1:size(omegafs,2)
+            GrNUMnm = A(n)/(-omegafs(m)^2 +1i*2*damp*omega*omegafs(m) + omega^2) + (Rhr(n)+1i*Rhi(n))/omegafs(m).^2 + Rlr(n)+1i*Rli(n);
+            valuenm = sum(sum(real(GrEXP(n,m)- GrNUMnm)^2 + imag(GrEXP(n,m)- GrNUMnm)^2));
+            % result = result + valuenm;
         end
     end
