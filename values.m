b06d0cfeb4c0e4453e133b16e706aa32624ac1f5
leomeunier <90618703+leomeunier@users.noreply.github.com>
2023-12-12 20:09:02 +0100

    Add files via upload

--- values.m
+++ values.m
@@ -1,11 +1,12 @@
 clear all
+clc 
 
 %properties
 red = [20e-3 1e-3];
 blue = [32e-3 1.5e-3];
 green = [50e-3 2.5e-3];
 
-Em = 7e11;
+Em = 7e10;
 rho = 2700;
 
 colors = [ red ; blue ; green];
@@ -15,15 +16,15 @@ EJ = zeros(1,3);
 i = 0;
 
 for i = 1:3
-    m(i)= rho*pi*(colors(i,1)^2 - (colors(i,1)-colors(i,2))^2)/4;
-    EA(i)= Em*pi*(colors(i,1)^2 - (colors(i,1)-colors(i,2))^2)/4;
-    EJ(i)= Em*(pi/2)*((colors(i,1)/2)^4 - ((colors(i,1)-colors(i,2))/2)^4);
+    m(i)= rho*pi*(colors(i,1)^2 - (colors(i,1)-2*colors(i,2))^2)/4;
+    EA(i)= Em*pi*(colors(i,1)^2 - (colors(i,1)-2*colors(i,2))^2)/4;
+    EJ(i)= Em/64 *pi * ((colors(i,1))^4 - ((colors(i,1)-2*colors(i,2))^4));
 end
 
 c2 = min(EJ./m);
 c = sqrt(c2);
 fmax = 200;
-Lmax = sqrt((1/(1.5*2*fmax))*c);
+Lmax = sqrt((pi/(2*2*fmax))*c);
 
 A = [0.7 0];
 B = [-0.42 0];
@@ -35,5 +36,11 @@ G = [-0.08 0.48];
 H = [-0.12 0.72];
 
 disp(norm(A-D));
-
-
+disp(norm(C-D));
+disp(norm(C-G));
+disp(norm(G-H));
+disp(norm(B-G));
+disp(norm(B-C));
+disp(norm(G-E));
+disp(norm(F-E));
+disp(norm(E-D));
