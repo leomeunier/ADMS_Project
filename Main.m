b987ce1b79dfb7243995aba6a41fb6b009554cf4
auroraprometti <152897155+auroraprometti@users.noreply.github.com>
2023-12-14 17:46:16 +0100

    Main.m

--- Main.m
+++ Main.m
@@ -18,16 +18,73 @@ dis_stru(posit,l,gamma,xy,pr,idb,ndof);
 [M,K] = assem(incid,l,m,EA,EJ,T,gamma,idb);
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%% Compute natural frequencies and mode shapes
-MFF = M(1:ndof,1:ndof);
-MCF = M(ndof+1:end,1:ndof);
-MFC = M(1:ndof,ndof+1:end);
-MCC = M(ndof+1:end,ndof+1:end);
+%% Add concentrated elements
 
-KFF = K(1:ndof,1:ndof);
-KCF = K(ndof+1:end,1:ndof);
-KFC = K(1:ndof,ndof+1:end);
-KCC = K(ndof+1:end,ndof+1:end);
+% Concentrated spring in node 1
+k = 4e+05;
+i_ndof_spring = idb(1,2);
+E_k1 = zeros(1,45);
+E_k1(:, i_ndof_spring) = 1;
+Kx1 = E_k1'*k*E_k1;
+
+% Concentrated spring in node 2
+k = 4e+05;
+i_ndof_spring = idb(2,2);
+E_k1 = zeros(1,45);
+E_k1(:, i_ndof_spring) = 1;
+Kx2 = E_k1'*k*E_k1;
+
+% Ktot
+Ktot = K + Kx1 + Kx2;
+
+% Concentrated masses
+m1 = 1; %[kg]
+m2 = 2; %[kg]
+m3 = 2; %[kg]
+m4 = 1.5; %[kg]
+
+% m1
+M1_h = diag([m1 m1 0]);
+i_ndof_m1 = idb(1,:);
+E_m1 = zeros(3,45);
+E_m1(:,i_ndof_m1) = eye(3);
+M1 = E_m1'*M1_h*E_m1;
+
+% m2
+M2_h = diag([m2 m2 0]);
+i_ndof_m2 = idb(3,:);
+E_m2 = zeros(3,45);
+E_m2(:,i_ndof_m2) = eye(3);
+M2 = E_m2'*M2_h*E_m2;
+
+% m3
+M3_h = diag([m3 m3 0]);
+i_ndof_m3 = idb(2,:);
+E_m3 = zeros(3,45);
+E_m3(:,i_ndof_m3) = eye(3);
+M3 = E_m3'*M3_h*E_m3;
+
+% m4
+M4_h = diag([m4 m4 0]);
+i_ndof_m4 = idb(6,:);
+E_m4 = zeros(3,45);
+E_m4(:,i_ndof_m4) = eye(3);
+M4 = E_m4'*M4_h*E_m4;
+
+% Mtot
+Mtot = M + M1 + M2 + M3 + M4;
+
+
+%% Compute natural frequencies and mode shapes
+MFF = Mtot(1:ndof,1:ndof);
+MCF = Mtot(ndof+1:end,1:ndof);
+MFC = Mtot(1:ndof,ndof+1:end);
+MCC = Mtot(ndof+1:end,ndof+1:end);
+
+KFF = Ktot(1:ndof,1:ndof);
+KCF = Ktot(ndof+1:end,1:ndof);
+KFC = Ktot(1:ndof,ndof+1:end);
+KCC = Ktot(ndof+1:end,ndof+1:end);
 
 [modes, omega] = eig(inv(MFF)*KFF);
 omega = sqrt(diag(omega));
@@ -106,6 +163,11 @@ subplot(2,1,2);
 plot(freq, angle(FRF));
 
 % Manca accelerazione
+
+
+
+
+
 
 
 
