5fdaa531ca4c9c2d032e222dfc3048b5dd655d4f
auroraprometti <152897155+auroraprometti@users.noreply.github.com>
2023-12-29 22:26:26 +0100

    ADMS_Assignment1.m

--- ADMS_Assignment1.m
+++ ADMS_Assignment1.m
@@ -35,8 +35,6 @@ semilogy(f,abs(dets), 'LineWidth',2)
 hold on 
 grid on
 
-% Searching for natural frequencies, so when determinant([H]) = 0
-
 gamma0 = [];
 
 for i = 2:length(dets)-1
@@ -63,16 +61,6 @@ end
 Hgamma = [];
 z = ones(4,4);
 
-% Solving H*z = 0
-% Since I found the first 4 omega0 that make det([H]) = 0, now the rows of
-% [H] are not linearly independent anymore. So I remove the first row and I
-% obtain H_new. While doing that I removed also the first column.
-% So now I consider it and call it N.
-% I obtained a new system to solve: H_new * z = - N
-% Note that the first row of z is still full of ones because is the row I
-% considered as known when at the beginning I've removed the first row of
-% the matrix [H].
-
 for i = 1:length(omega0)
     Hgamma = H(gamma0(i));
     H_new = Hgamma(2:end, 2:end);
@@ -82,8 +70,6 @@ for i = 1:length(omega0)
     % z is a matrix in which each column is a vector with the coefficients
     % for each natural frequency.
 end
-
-% I have w = phi(x) * G(t), but I only want the mode shapes so phi(x).
 
 for i = 1:4
     phi = @(x) z(1,i)*cos(gamma0(i)*x) + z(2,i)*sin(gamma0(i)*x) + z(3,i)*cosh(gamma0(i)*x) + z(4,i)*sinh(gamma0(i)*x);
@@ -109,7 +95,6 @@ for i = 1:4
         phi4 = phi;
     end
 
-    
     % I obtained the mode shapes corresponding to the first 4 natural
     % frequencies of the system.
 
@@ -171,38 +156,60 @@ grid on
 % controllability: place the force in a vibration node (x_k)
 % reciprocity
 
+% Altre G_jk
+xk=[1.2;0.2;0.605;1];
+xj=[0.2;1.2;0.8;0.605];
+
+
+for i = 2:length(xj)
+    figure(5+i)
+    subplot(2,1,1)
+    semilogy(OM_vect/2/pi, abs(G_jk(OM_vect,xj(i),xk(i))), 'LineWidth', 2)
+    xlabel('Frequency [Hz]')
+    ylabel('[m/N]'); 
+   % legend('G_{jk} ( \Omega ) = x_{j}/F_{k}, x_{j} = %g , x_{k} = %g', xj(i),xk(i))
+    grid on; hold on;
+    subplot(2,1,2)
+    plot(OM_vect/2/pi, angle(G_jk(OM_vect,xj(i),xk(i))), 'LineWidth',2)
+    grid on
+    hold on
+end
+
+
 
 %% Numerical Frequency Response Function - Comparison with the one on slide 12
 
 % MODE 1 
 
-x_j = 0.2;
-x_k = 1.2; 
+x_k=[1.2;0.2;0.605;1];
+x_j=[0.2;1.2;0.8;0.605];
+
+for s = 1:length(x_j)
 
 G_r_num =  @ (OM, om_i, csi_i, A_jk_i, Rlow_re, Rlow_im, Rhigh_re, Rhigh_im)  A_jk_i./(-OM.^2+1j*2*csi_i.*om_i.*OM+om_i.^2) + ...
              (Rlow_re+1j*Rlow_im)./OM.^2 + Rhigh_re+1j*Rhigh_im;
 
 OM_vect = 2*pi*(4:0.005:5); 
 
-err = @ (X) sum(real(G_jk(OM_vect, x_j, x_k)- G_r_num(OM_vect, X(1), X(2), X(3), X(4), X(5), X(6), X(7))).^2 + ...
-            imag(G_jk(OM_vect, x_j, x_k)- G_r_num(OM_vect, X(1), X(2), X(3), X(4), X(5), X(6), X(7))).^2);
+err = @ (X) sum(real(G_jk(OM_vect, x_j(s), x_k(s))- G_r_num(OM_vect, X(1), X(2), X(3), X(4), X(5), X(6), X(7))).^2 + ...
+            imag(G_jk(OM_vect, x_j(s), x_k(s))- G_r_num(OM_vect, X(1), X(2), X(3), X(4), X(5), X(6), X(7))).^2);
 
 % Initial guesses
 
 % omegai
-[~,om_index] = max(abs(G_jk(OM_vect, x_j, x_k))); % find the index for which I have the maximum value of the FRF
+[~,om_index] = max(abs(G_jk(OM_vect, x_j(s), x_k(s)))); % find the index for which I have the maximum value of the FRF
 om_i_0 = OM_vect(om_index);
 
 % Damping ratio
 % csi = - 1 / ( om_i_0 * dphi/dbigomega|om_i_0 );
-value1 = angle(G_jk(om_i_0-0.005, x_j, x_k));
-value2 = angle(G_jk(om_i_0+0.005, x_j, x_k));
+value1 = angle(G_jk(om_i_0-0.005, x_j(s), x_k(s)));
+value2 = angle(G_jk(om_i_0+0.005, x_j(s), x_k(s)));
 m = (value2-value1)/((om_i_0 + 0.005)-(om_i_0-0.005));
 csi_0 = -1/(om_i_0*m);
 
 % A_jk
 % At the peak we can approximate G_num as Ajk/(2j*csi0*om_i_0^2)
-A_jk_i_0 = imag(max(G_jk(OM_vect, x_j, x_k)))*1i * 2*1i*csi_0*om_i_0^2;
+A_jk_i_0 = imag(max(G_jk(OM_vect, x_j(s), x_k(s))))*1i * 2*1i*csi_0*om_i_0^2;
 
 % Residuals = 0
 % Vector of initial guesses
@@ -211,7 +218,7 @@ x0 = [om_i_0; csi_0; A_jk_i_0; 0; 0; 0; 0];
 
 X = lsqnonlin(err,x0, [],[],[]);
 
-figure (6)
+figure (5+s)
 subplot(2,1,1)
 hold on
 semilogy(OM_vect/2/pi, abs(G_r_num(OM_vect, X(1), X(2), X(3), X(4), X(5), X(6), X(7))),'ro','MarkerSize',8)
@@ -220,371 +227,157 @@ hold on
 grid on
 plot(OM_vect/2/pi, angle(G_r_num(OM_vect,X(1), X(2), X(3), X(4), X(5), X(6), X(7))),'ro','MarkerSize', 8)
 
+end
 
-%% FREQUENCY RESPONSE FUNCTIONS for a given set of experimental FRFs
+% MODE 2 
 
-% First mode
-% 3 FRFs, to compare with slide 13
+x_k=[1.2;0.2;0.605;1];
+x_j=[0.2;1.2;0.8;0.605];
 
-x_j = [0.2, 0.42, 0.9];
-x_k = 1.2;
+for s = 1:length(x_j)
 
-% G_r_num =  @ (OM, om_i, csi_i, A_jk_i, Rlow_re, Rlow_im, Rhigh_re, Rhigh_im)  A_jk_i./(-OM.^2+1j*2*csi_i.*om_i.*OM+om_i.^2) + ...
-             %(Rlow_re+1j*Rlow_im)./OM.^2 + Rhigh_re+1j*Rhigh_im;
-G_r_num1 = @ (OM, om_i, csi_i, xx) (xx(:,1)./(-OM.^2+1i*2*csi_i.*om_i.*OM+om_i.^2) + ...
-             (xx(:,2)+1i*xx(:,4))./OM.^2 + xx(:,3)+1i*xx(:,5));
-% By doing this I obtain:
-% xx = [A_jk_1, Rlow_re1, Rlow_im1, Rhigh_re1, Rlow_im1]
-%      ....
-%      [A_jk_r, Rlow_rer, Rlow_imr, Rhigh_rer, Rlow_imr]
-% r = number of x_j x_k couples
+G_r_num =  @ (OM, om_i, csi_i, A_jk_i, Rlow_re, Rlow_im, Rhigh_re, Rhigh_im)  A_jk_i./(-OM.^2+1j*2*csi_i.*om_i.*OM+om_i.^2) + ...
+             (Rlow_re+1j*Rlow_im)./OM.^2 + Rhigh_re+1j*Rhigh_im;
 
-OM_vect = 2*pi*(4:0.005:5);
-l = length(x_j);
-err_r = @(x)sum(sum(real(G_jk(OM_vect,x_j,x_k) - G_r_num1(OM_vect, x(1,1),x(1,2), x(:,3:7))).^2 + ...
-    imag(G_jk(OM_vect,x_j,x_k) - G_r_num1(OM_vect,x(1,1),x(1,2), x(:,3:7))).^2));
+OM_vect_2 = 2*pi*(27.8:0.005:28.8);
 
-x_0 = zeros(length(x_j), 7);
+err = @ (X) sum(real(G_jk(OM_vect_2, x_j(s), x_k(s))- G_r_num(OM_vect_2, X(1), X(2), X(3), X(4), X(5), X(6), X(7))).^2 + ...
+            imag(G_jk(OM_vect_2, x_j(s), x_k(s))- G_r_num(OM_vect_2, X(1), X(2), X(3), X(4), X(5), X(6), X(7))).^2);
 
 % Initial guesses
 
 % omegai
-[~,om_index] = max(abs(G_jk(OM_vect, x_j(1), x_k))); % find the index for which I have the maximum value of the FRF
-% Since omegai is the same for each FRF, I just consider 1.
-om_i_0 = OM_vect(om_index);
-
-% Damping ratio
-% csi = - 1 / ( om_i_0 * dphi/dbigomega|om_i_0 );
-value1 = angle(G_jk(om_i_0-0.005, x_j(1), x_k));
-value2 = angle(G_jk(om_i_0+0.005, x_j(1), x_k));
-m = (value2-value1)/((om_i_0 + 0.005)-(om_i_0-0.005));
-csi_0 = -1/(om_i_0*m); % Just one because is the same for all FRFs
-
-% A_jk
-% At the peak we can approximate G_num as Ajk/(2j*csi0*om_i_0^2)
-A_jk_i_0 = zeros(length(x_j), 1);
-
-for ii = 1:length(x_j)
-    A_jk_i_0(ii) = imag(max(G_jk(OM_vect, x_j(ii),x_k)))*1i * 2*1i*csi_0*om_i_0^2;
-end
-
-% Residuals = 0
-
-% Vector of initial guesses
-x_0(:,1) = om_i_0;
-x_0(:,2) = csi_0;
-x_0(:,3) = A_jk_i_0;
-
-X = lsqnonlin(err_r,x_0,[],[],[]);
-
-% I compute another graph for G_jk in order to compare this set of FRFs to
-% the one found in the previous step
-OM_vect2 = 2*pi*(0:0.02:200);
-
-figure (7)
-subplot(2,1,1)
-semilogy(OM_vect2/2/pi, abs(G_jk(OM_vect2,x_j1,x_k1)), 'LineWidth', 2)
-ylim([1e-6 0.3e-1])
-xlabel('Frequency [Hz]')
-ylabel('[m/N]'); 
-legend('G_{jk} ( \Omega ) = x_{j}/F_{k}, x_{j} = 0.2 m , x_{k} = 1.2 m')
-grid on; hold on;
-
-subplot(2,1,2)
-plot(OM_vect2/2/pi, angle(G_jk(OM_vect2,x_j1,x_k1)), 'LineWidth',2)
-grid on
-
-% Now I plot my set of FRFS
-
-figure (7)
-subplot(2,1,1)
-hold on
-semilogy(OM_vect/2/pi, abs(G_r_num1(OM_vect, X(1,1),X(1,2), X(1,3:7))), 'go','MarkerSize',8) %I take only the first row because I'm comparing it with
-% G_jk which had 0.2 as x_j
-
-%Plot of initial guesses
-%semilogy(OM_vect/2/pi, abs(G_r_num1(OM_vect, x_0(1,1),x_0(1,2), x_0(1,3:7))),'bx', 'MarkerSize', 8)
-
-subplot(2,1,2)
-hold on
-grid on
-plot(OM_vect/2/pi, angle(G_r_num1(OM_vect, X(1,1),X(1,2), X(1,3:7))),'go','MarkerSize', 8)
-
-% Plot of initial guesses
-%plot(OM_vect/2/pi, angle(G_r_num1(OM_vect, x_0(1,1),x_0(1,2), x_0(1,3:7))),'bx', 'MarkerSize', 8)
-
-% Plot the identified mode shapes 
-M = [];
-for ii = 1:length(x_j)
-    [~, I] = max(abs(G_r_num1(OM_vect, X(1,1),X(1,2), X(ii,3:7))));
-    M = [M; imag(G_r_num1(OM_vect(I), X(1,1),X(1,2), X(ii,3:7)))];
-end
-
-M = M * phi1(0.2)/M(1)/(max(abs(phi1(distance))));
-figure (2)
-plot(x_j,M,'ro', 'MarkerSize', 8, 'LineWidth',2)
-
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-% Second mode
-% 3 FRFs, to compare with slide 13
-
-x_j = [0.2, 0.42, 0.9];
-x_k = 1.2;
-
-% G_r_num =  @ (OM, om_i, csi_i, A_jk_i, Rlow_re, Rlow_im, Rhigh_re, Rhigh_im)  A_jk_i./(-OM.^2+1j*2*csi_i.*om_i.*OM+om_i.^2) + ...
-             %(Rlow_re+1j*Rlow_im)./OM.^2 + Rhigh_re+1j*Rhigh_im;
-G_r_num1 = @ (OM, om_i, csi_i, xx) (xx(:,1)./(-OM.^2+1i*2*csi_i.*om_i.*OM+om_i.^2) + ...
-             (xx(:,2)+1i*xx(:,4))./OM.^2 + xx(:,3)+1i*xx(:,5));
-% By doing this I obtain:
-% xx = [A_jk_1, Rlow_re1, Rlow_im1, Rhigh_re1, Rlow_im1]
-%      ....
-%      [A_jk_r, Rlow_rer, Rlow_imr, Rhigh_rer, Rlow_imr]
-% r = number of x_j x_k couples
-
-OM_vect_2 = 2*pi*(27.8:0.005:28.7);
-l = length(x_j);
-err_r = @(x)sum(sum(real(G_jk(OM_vect_2,x_j,x_k) - G_r_num1(OM_vect_2, x(1,1),x(1,2), x(:,3:7))).^2 + ...
-    imag(G_jk(OM_vect_2,x_j,x_k) - G_r_num1(OM_vect_2,x(1,1),x(1,2), x(:,3:7))).^2));
-
-x_0 = zeros(length(x_j), 7);
-
-% Initial guesses
-
-% omegai
-[~,om_index] = max(abs(G_jk(OM_vect_2, x_j(1), x_k))); % find the index for which I have the maximum value of the FRF
-% Since omegai is the same for each FRF, I just consider 1.
+[~,om_index] = max(abs(G_jk(OM_vect_2, x_j(s), x_k(s)))); % find the index for which I have the maximum value of the FRF
 om_i_0 = OM_vect_2(om_index);
 
 % Damping ratio
 % csi = - 1 / ( om_i_0 * dphi/dbigomega|om_i_0 );
-value1 = angle(G_jk(om_i_0-0.005, x_j(1), x_k));
-value2 = angle(G_jk(om_i_0+0.005, x_j(1), x_k));
+value1 = angle(G_jk(om_i_0-0.005, x_j(s), x_k(s)));
+value2 = angle(G_jk(om_i_0+0.005, x_j(s), x_k(s)));
 m = (value2-value1)/((om_i_0 + 0.005)-(om_i_0-0.005));
-csi_0 = -1/(om_i_0*m); % Just one because is the same for all FRFs
+csi_0 = -1/(om_i_0*m);
 
 % A_jk
 % At the peak we can approximate G_num as Ajk/(2j*csi0*om_i_0^2)
-A_jk_i_0 = zeros(length(x_j), 1);
-
-for ii = 1:length(x_j)
-    A_jk_i_0(ii) = imag(max(G_jk(OM_vect_2, x_j(ii),x_k)))*1i * 2*1i*csi_0*om_i_0^2;
-end
+A_jk_i_0 = imag(max(G_jk(OM_vect_2, x_j(s), x_k(s))))*1i * 2*1i*csi_0*om_i_0^2;
 
 % Residuals = 0
-
 % Vector of initial guesses
-x_0(:,1) = om_i_0;
-x_0(:,2) = csi_0;
-x_0(:,3) = A_jk_i_0;
+x0 = [om_i_0; csi_0; A_jk_i_0; 0; 0; 0; 0];
 
-X = lsqnonlin(err_r,x_0,[],[],[]);
 
-figure (7)
+X = lsqnonlin(err,x0, [],[],[]);
+
+figure (5+s)
 subplot(2,1,1)
 hold on
-semilogy(OM_vect_2/2/pi, abs(G_r_num1(OM_vect_2, X(1,1),X(1,2), X(1,3:7))), 'go','MarkerSize',8) %I take only the first row because I'm comparing it with
-% G_jk which had 0.2 as x_j
-
-%Plot of initial guesses
-%semilogy(OM_vect/2/pi, abs(G_r_num1(OM_vect_2, x_0(1,1),x_0(1,2), x_0(1,3:7))),'bx', 'MarkerSize', 8)
-
+semilogy(OM_vect_2/2/pi, abs(G_r_num(OM_vect_2, X(1), X(2), X(3), X(4), X(5), X(6), X(7))),'ro','MarkerSize',8)
 subplot(2,1,2)
 hold on
 grid on
-plot(OM_vect_2/2/pi, angle(G_r_num1(OM_vect_2, X(1,1),X(1,2), X(1,3:7))),'go','MarkerSize', 8)
+plot(OM_vect_2/2/pi, angle(G_r_num(OM_vect_2,X(1), X(2), X(3), X(4), X(5), X(6), X(7))),'ro','MarkerSize', 8)
 
-% Plot of initial guesses
-% plot(OM_vect/2/pi, angle(G_r_num1(OM_vect, x_0(1,1),x_0(1,2), x_0(1,3:7))),'bx', 'MarkerSize', 8)
-
-% Plot the identified mode shapes 
-M = [];
-for ii = 1:length(x_j)
-    [~, I] = max(abs(G_r_num1(OM_vect_2, X(1,1),X(1,2), X(ii,3:7))));
-    M = [M; imag(G_r_num1(OM_vect_2(I), X(1,1),X(1,2), X(ii,3:7)))];
 end
 
-M = M*phi2(0.2)/M(1)/(max(abs(phi2(distance))));
-figure (3)
-plot(x_j,M,'ro', 'MarkerSize', 8, 'LineWidth',2)
+% MODE 3
 
+x_k=[1.2;0.2;0.605;1];
+x_j=[0.2;1.2;0.8;0.605];
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+for s = 1:length(x_j)
 
-% Third mode
-% 3 FRFs, to compare with slide 13
-
-x_j = [0.2, 0.42, 0.9];
-x_k = 1.2;
-
-% G_r_num =  @ (OM, om_i, csi_i, A_jk_i, Rlow_re, Rlow_im, Rhigh_re, Rhigh_im)  A_jk_i./(-OM.^2+1j*2*csi_i.*om_i.*OM+om_i.^2) + ...
-             %(Rlow_re+1j*Rlow_im)./OM.^2 + Rhigh_re+1j*Rhigh_im;
-G_r_num1 = @ (OM, om_i, csi_i, xx) (xx(:,1)./(-OM.^2+1i*2*csi_i.*om_i.*OM+om_i.^2) + ...
-             (xx(:,2)+1i*xx(:,4))./OM.^2 + xx(:,3)+1i*xx(:,5));
-% By doing this I obtain:
-% xx = [A_jk_1, Rlow_re1, Rlow_im1, Rhigh_re1, Rlow_im1]
-%      ....
-%      [A_jk_r, Rlow_rer, Rlow_imr, Rhigh_rer, Rlow_imr]
-% r = number of x_j x_k couples
+G_r_num =  @ (OM, om_i, csi_i, A_jk_i, Rlow_re, Rlow_im, Rhigh_re, Rhigh_im)  A_jk_i./(-OM.^2+1j*2*csi_i.*om_i.*OM+om_i.^2) + ...
+             (Rlow_re+1j*Rlow_im)./OM.^2 + Rhigh_re+1j*Rhigh_im;
 
 OM_vect_3 = 2*pi*(78.5:0.005:79.5);
-l = length(x_j);
-err_r = @(x)sum(sum(real(G_jk(OM_vect_3,x_j,x_k) - G_r_num1(OM_vect_3, x(1,1),x(1,2), x(:,3:7))).^2 + ...
-    imag(G_jk(OM_vect_3,x_j,x_k) - G_r_num1(OM_vect_3,x(1,1),x(1,2), x(:,3:7))).^2));
 
-x_0 = zeros(length(x_j), 7);
+err = @ (X) sum(real(G_jk(OM_vect_3, x_j(s), x_k(s))- G_r_num(OM_vect_3, X(1), X(2), X(3), X(4), X(5), X(6), X(7))).^2 + ...
+            imag(G_jk(OM_vect_3, x_j(s), x_k(s))- G_r_num(OM_vect_3, X(1), X(2), X(3), X(4), X(5), X(6), X(7))).^2);
 
 % Initial guesses
 
 % omegai
-[~,om_index] = max(abs(G_jk(OM_vect_3, x_j(1), x_k))); % find the index for which I have the maximum value of the FRF
-% Since omegai is the same for each FRF, I just consider 1.
+[~,om_index] = max(abs(G_jk(OM_vect_3, x_j(s), x_k(s)))); % find the index for which I have the maximum value of the FRF
 om_i_0 = OM_vect_3(om_index);
 
 % Damping ratio
 % csi = - 1 / ( om_i_0 * dphi/dbigomega|om_i_0 );
-value1 = angle(G_jk(om_i_0-0.005, x_j(1), x_k));
-value2 = angle(G_jk(om_i_0+0.005, x_j(1), x_k));
+value1 = angle(G_jk(om_i_0-0.005, x_j(s), x_k(s)));
+value2 = angle(G_jk(om_i_0+0.005, x_j(s), x_k(s)));
 m = (value2-value1)/((om_i_0 + 0.005)-(om_i_0-0.005));
-csi_0 = -1/(om_i_0*m); % Just one because is the same for all FRFs
+csi_0 = -1/(om_i_0*m);
 
 % A_jk
 % At the peak we can approximate G_num as Ajk/(2j*csi0*om_i_0^2)
-A_jk_i_0 = zeros(length(x_j), 1);
-
-for ii = 1:length(x_j)
-    A_jk_i_0(ii) = imag(max(G_jk(OM_vect_3, x_j(ii),x_k)))*1i * 2*1i*csi_0*om_i_0^2;
-end
+A_jk_i_0 = imag(max(G_jk(OM_vect_3, x_j(s), x_k(s))))*1i * 2*1i*csi_0*om_i_0^2;
 
 % Residuals = 0
-
 % Vector of initial guesses
-x_0(:,1) = om_i_0;
-x_0(:,2) = csi_0;
-x_0(:,3) = A_jk_i_0;
+x0 = [om_i_0; csi_0; A_jk_i_0; 0; 0; 0; 0];
 
-X = lsqnonlin(err_r,x_0,[],[],[]);
 
-figure (7)
+X = lsqnonlin(err,x0, [],[],[]);
+
+figure (5+s)
 subplot(2,1,1)
 hold on
-semilogy(OM_vect_3/2/pi, abs(G_r_num1(OM_vect_3, X(1,1),X(1,2), X(1,3:7))), 'go','MarkerSize',8) %I take only the first row because I'm comparing it with
-% G_jk which had 0.2 as x_j
-
-%Plot of initial guesses
-%semilogy(OM_vect/2/pi, abs(G_r_num1(OM_vect_2, x_0(1,1),x_0(1,2), x_0(1,3:7))),'bx', 'MarkerSize', 8)
-
+semilogy(OM_vect_3/2/pi, abs(G_r_num(OM_vect_3, X(1), X(2), X(3), X(4), X(5), X(6), X(7))),'ro','MarkerSize',8)
 subplot(2,1,2)
 hold on
 grid on
-plot(OM_vect_3/2/pi, angle(G_r_num1(OM_vect_3, X(1,1),X(1,2), X(1,3:7))),'go','MarkerSize', 8)
+plot(OM_vect_3/2/pi, angle(G_r_num(OM_vect_3,X(1), X(2), X(3), X(4), X(5), X(6), X(7))),'ro','MarkerSize', 8)
 
-% Plot of initial guesses
-% plot(OM_vect/2/pi, angle(G_r_num1(OM_vect, x_0(1,1),x_0(1,2), x_0(1,3:7))),'bx', 'MarkerSize', 8)
-
-% Plot the identified mode shapes 
-M = [];
-for ii = 1:length(x_j)
-    [~, I] = max(abs(G_r_num1(OM_vect_3, X(1,1),X(1,2), X(ii,3:7))));
-    M = [M; imag(G_r_num1(OM_vect_3(I), X(1,1),X(1,2), X(ii,3:7)))];
 end
 
-M = M*phi3(0.2)/M(1)/(max(abs(phi3(distance))));
-figure (4)
-plot(x_j,M,'ro', 'MarkerSize', 8, 'LineWidth',2)
+% MODE 4
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+x_k=[1.2;0.2;0.605;1];
+x_j=[0.2;1.2;0.8;0.605];
 
-% Fourth mode
-% 3 FRFs, to compare with slide 13
+for s = 1:length(x_j)
 
-x_j = [0.2, 0.42, 0.9];
-x_k = 1.2;
-
-% G_r_num =  @ (OM, om_i, csi_i, A_jk_i, Rlow_re, Rlow_im, Rhigh_re, Rhigh_im)  A_jk_i./(-OM.^2+1j*2*csi_i.*om_i.*OM+om_i.^2) + ...
-             %(Rlow_re+1j*Rlow_im)./OM.^2 + Rhigh_re+1j*Rhigh_im;
-G_r_num1 = @ (OM, om_i, csi_i, xx) (xx(:,1)./(-OM.^2+1i*2*csi_i.*om_i.*OM+om_i.^2) + ...
-             (xx(:,2)+1i*xx(:,4))./OM.^2 + xx(:,3)+1i*xx(:,5));
-% By doing this I obtain:
-% xx = [A_jk_1, Rlow_re1, Rlow_im1, Rhigh_re1, Rlow_im1]
-%      ....
-%      [A_jk_r, Rlow_rer, Rlow_imr, Rhigh_rer, Rlow_imr]
-% r = number of x_j x_k couples
+G_r_num =  @ (OM, om_i, csi_i, A_jk_i, Rlow_re, Rlow_im, Rhigh_re, Rhigh_im)  A_jk_i./(-OM.^2+1j*2*csi_i.*om_i.*OM+om_i.^2) + ...
+             (Rlow_re+1j*Rlow_im)./OM.^2 + Rhigh_re+1j*Rhigh_im;
 
 OM_vect_4 = 2*pi*(154.03:0.005:155.5);
-l = length(x_j);
-err_r = @(x)sum(sum(real(G_jk(OM_vect_4,x_j,x_k) - G_r_num1(OM_vect_4, x(1,1),x(1,2), x(:,3:7))).^2 + ...
-    imag(G_jk(OM_vect_4,x_j,x_k) - G_r_num1(OM_vect_4,x(1,1),x(1,2), x(:,3:7))).^2));
 
-x_0 = zeros(length(x_j), 7);
+err = @ (X) sum(real(G_jk(OM_vect_4, x_j(s), x_k(s))- G_r_num(OM_vect_4, X(1), X(2), X(3), X(4), X(5), X(6), X(7))).^2 + ...
+            imag(G_jk(OM_vect_4, x_j(s), x_k(s))- G_r_num(OM_vect_4, X(1), X(2), X(3), X(4), X(5), X(6), X(7))).^2);
 
 % Initial guesses
 
 % omegai
-[~,om_index] = max(abs(G_jk(OM_vect_4, x_j(1), x_k))); % find the index for which I have the maximum value of the FRF
-% Since omegai is the same for each FRF, I just consider 1.
+[~,om_index] = max(abs(G_jk(OM_vect_4, x_j(s), x_k(s)))); % find the index for which I have the maximum value of the FRF
 om_i_0 = OM_vect_4(om_index);
 
 % Damping ratio
 % csi = - 1 / ( om_i_0 * dphi/dbigomega|om_i_0 );
-value1 = angle(G_jk(om_i_0-0.005, x_j(1), x_k));
-value2 = angle(G_jk(om_i_0+0.005, x_j(1), x_k));
+value1 = angle(G_jk(om_i_0-0.005, x_j(s), x_k(s)));
+value2 = angle(G_jk(om_i_0+0.005, x_j(s), x_k(s)));
 m = (value2-value1)/((om_i_0 + 0.005)-(om_i_0-0.005));
-csi_0 = -1/(om_i_0*m); % Just one because is the same for all FRFs
+csi_0 = -1/(om_i_0*m);
 
 % A_jk
 % At the peak we can approximate G_num as Ajk/(2j*csi0*om_i_0^2)
-A_jk_i_0 = zeros(length(x_j), 1);
-
-for ii = 1:length(x_j)
-    A_jk_i_0(ii) = imag(max(G_jk(OM_vect_4, x_j(ii),x_k)))*1i * 2*1i*csi_0*om_i_0^2;
-end
+A_jk_i_0 = imag(max(G_jk(OM_vect_4, x_j(s), x_k(s))))*1i * 2*1i*csi_0*om_i_0^2;
 
 % Residuals = 0
-
 % Vector of initial guesses
-x_0(:,1) = om_i_0;
-x_0(:,2) = csi_0;
-x_0(:,3) = A_jk_i_0;
+x0 = [om_i_0; csi_0; A_jk_i_0; 0; 0; 0; 0];
 
-X = lsqnonlin(err_r,x_0,[],[],[]);
 
-figure (7)
+X = lsqnonlin(err,x0, [],[],[]);
+
+figure (5+s)
 subplot(2,1,1)
 hold on
-semilogy(OM_vect_4/2/pi, abs(G_r_num1(OM_vect_4, X(1,1),X(1,2), X(1,3:7))), 'go','MarkerSize',8) %I take only the first row because I'm comparing it with
-% G_jk which had 0.2 as x_j
-
-%Plot of initial guesses
-%semilogy(OM_vect/2/pi, abs(G_r_num1(OM_vect_2, x_0(1,1),x_0(1,2), x_0(1,3:7))),'bx', 'MarkerSize', 8)
-
+semilogy(OM_vect_4/2/pi, abs(G_r_num(OM_vect_4, X(1), X(2), X(3), X(4), X(5), X(6), X(7))),'ro','MarkerSize',8)
 subplot(2,1,2)
 hold on
 grid on
-plot(OM_vect_4/2/pi, angle(G_r_num1(OM_vect_4, X(1,1),X(1,2), X(1,3:7))),'go','MarkerSize', 8)
+plot(OM_vect_4/2/pi, angle(G_r_num(OM_vect_4,X(1), X(2), X(3), X(4), X(5), X(6), X(7))),'ro','MarkerSize', 8)
 
-% Plot of initial guesses
-% plot(OM_vect/2/pi, angle(G_r_num1(OM_vect, x_0(1,1),x_0(1,2), x_0(1,3:7))),'bx', 'MarkerSize', 8)
-
-% Plot the identified mode shapes 
-M = [];
-for ii = 1:length(x_j)
-    [~, I] = max(abs(G_r_num1(OM_vect_4, X(1,1),X(1,2), X(ii,3:7))));
-    M = [M; imag(G_r_num1(OM_vect_4(I), X(1,1),X(1,2), X(ii,3:7)))];
 end
-
-M = M*phi4(0.2)/M(1)/(max(abs(phi4(distance))));
-figure (5)
-plot(x_j,M,'ro', 'MarkerSize', 8, 'LineWidth',2)
-
-
-
-%% NOTES
-
-% Consider more than 3 FRFs and maybe change position of the force
-% Fix the legends 
-% Table comparing model with identified as shown in slide 13
 
 
 
